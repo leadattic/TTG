d1e6523b4d023050177617c004c574439df78a74
leadattic <90087384+leadattic@users.noreply.github.com>
2023-08-13 19:03:37 +0000

    Initial commit

---
+++ django_project/urls.py
@@ -0,0 +1,31 @@
+"""django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views, api
+
+urlpatterns = [
+    path('', views.home),
+    path('games/', views.games),
+    path('hatlista/', views.hatlista),
+    path('api/', views.api_docs),
+    path('api/v1/', views.api),
+    path('styles/get_style/', views.home_style),
+    path('styles/home/', views.home_style),
+    path('dashboard/', views.dashboard),
+    path('js/', views.js)
+    
+]
---
+++ django_project/views.py
@@ -0,0 +1,121 @@
+from django.http import HttpResponse
+from django.template import loader
+import os
+import json
+
+
+
+def home(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def home_style(request):
+    return HttpResponse("""body { 
+    
+    font-family: sans-serif;
+    background-color: #ededed;
+    color: #d6d6d6;
+    background-image: url(
+    "ttglogo.png");
+    background-size: 100%, 100%;
+  
+}img.resize {
+    width: 100%; /* you can use % */
+    height: 100%;
+}
+#centralize{
+    text-align: center;
+
+}
+.card{
+    margin: 20px;
+    border: 3px solid #C5EAFA ;
+    background-color: #ff4785;
+    width: 200px;
+    height: 260px;
+    border-radius: 12px;
+}
+#mbr{
+    margin-left: 55px;
+    margin-top: 65px;
+}""")
+
+
+def games(request):
+    template = loader.get_template('games.html')
+    return HttpResponse(template.render())
+
+
+def dashboard(request):
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render())
+
+
+def api_docs(request):  # /api leads to this function
+    template = loader.get_template('api.html')
+    return HttpResponse(template.render())
+
+
+def api(request):
+    return HttpResponse("paths", status=200)
+
+
+def hatlista(request):
+    tried_pass = request.GET.get("pass")
+    if tried_pass == os.getenv("PASSWORD"):
+        return HttpResponse(os.getenv("HATADE"))
+    else:
+        return HttpResponse("""
+        <!DOCTYPE html>
+        <html>
+
+        <script>
+        alert("Incorrect");
+        location="https://newttg.leadattic953788.repl.co/#"
+        </script>
+        </html>
+
+    
+        
+        """)
+
+
+def get_style(request):
+    name = request.GET.get("name").upper()
+    styles_raw = os.environ["STYLE-" + name]
+
+    try:
+        return HttpResponse(json.dumps({"response": styles_raw}), status=200)
+    except KeyError:
+        print(f"404: Style ${name} not found")
+        return HttpResponse(f"404: Style ${name} not found", status=404)
+
+
+def js(request):
+    # No longer in use as static files have been determined to be a better option. Therefore 410 Gone.
+    """
+    ame = request.GET.get("name")
+    base_path = "django_project/templates/"
+    complete_path = base_path + name + ".js"
+    file = open(complete_path, "r")
+    content = file.read()
+    file.close()
+    return HttpResponse(content, status=200)
+    """
+    return HttpResponse("410: Gone", status=410)
+
+
+# Custom 404 & 500 pages
+def handler404(request, *args, **argv):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
